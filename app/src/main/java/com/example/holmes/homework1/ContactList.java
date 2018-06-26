1fddc9f311010259df5e26fdb85180b55e8bcdef
Michael Holmes <mholmes311@gmail.com>
2018-06-25 21:08:37 -0400

    Code is somewhat broken. Adapter is not fully completed yet.

--- app/src/main/java/com/example/holmes/homework1/ContactList.java
+++ app/src/main/java/com/example/holmes/homework1/ContactList.java
@@ -2,13 +2,50 @@ package com.example.holmes.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class ContactList extends AppCompatActivity {
+
+    ListView listView;
+
+    //Example array
+    String[] nameArray = {"Octopus","Pig","Sheep","Rabbit","Snake","Spider" };
+
+    //Example array
+    String[] infoArray = {
+            "8 tentacled monster",
+            "Delicious in rolls",
+            "Great for jumpers",
+            "Nice in a stew",
+            "Great for shoes",
+            "Scary."
+    };
+
+    //Example array
+    Integer[] imageArray = {R.drawable.octopus,
+            R.drawable.pig,
+            R.drawable.sheep,
+            R.drawable.rabbit,
+            R.drawable.snake,
+            R.drawable.spider};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
         setTitle("Contact List");
+
+        CustomListAdapter listAdapter = new CustomListAdapter(this, nameArray, infoArray, imageArray);
+        listView = (ListView) findViewById(R.id.lvContactList);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
